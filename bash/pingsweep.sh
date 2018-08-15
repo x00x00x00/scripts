3bbcbf40d5d3fb9c6f4a37ed3a22c95c3318908c
x00x00x00 <42035924+x00x00x00@users.noreply.github.com>
2018-08-15 13:49:56 +0200

    Update pingsweep.sh

--- bash/pingsweep.sh
+++ bash/pingsweep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in {1..254} ; do (ping -c 1 10.11.1.$i | grep "ttl" | cut -d " " -f4 | cut -d ":" -f1 &); done
+for i in {1..254} ; do (ping -c 1 192.168.1.$i | grep "ttl" | cut -d " " -f4 | cut -d ":" -f1 &); done
